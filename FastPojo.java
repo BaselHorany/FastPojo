9bbbde92315d67a4aad5817bd6b0fe1aa46ff62e
BaselHorany <30597730+BaselHorany@users.noreply.github.com>
2017-08-06 15:23:22 +0300

    Add files via upload

---
+++ FastPojo.java
@@ -0,0 +1,418 @@
+package com.used.learngr.modle;
+
+public class FastPojo {
+    private String s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19;
+    private Integer i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15,i16,i17,i18,i19;
+    private Double d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18,d19;
+    private Boolean b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19;
+
+    public FastPojo() {
+    }
+
+    public FastPojo(
+            String s1,String s2,String s3,String s4,String s5,String s6,String s7,String s8,String s9,String s10,String s11,String s12,String s13,String s14,String s15,String s16,String s17,String s18,String s19,
+            Integer i1,Integer i2,Integer i3,Integer i4,Integer i5,Integer i6,Integer i7,Integer i8,Integer i9,Integer i10,Integer i11,Integer i12,Integer i13,Integer i14,Integer i15,Integer i16,Integer i17,Integer i18,Integer i19,
+            Double d1,Double d2,Double d3,Double d4,Double d5,Double d6,Double d7,Double d8,Double d9,Double d10,Double d11,Double d12,Double d13,Double d14,Double d15,Double d16,Double d17,Double d18,Double d19,
+            Boolean b1,Boolean b2,Boolean b3,Boolean b4,Boolean b5,Boolean b6,Boolean b7,Boolean b8,Boolean b9,Boolean b10,Boolean b11,Boolean b12,Boolean b13,Boolean b14,Boolean b15,Boolean b16,Boolean b17,Boolean b18,Boolean b19) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        this.s4 = s4;
+        this.s5 = s5;
+        this.s6 = s6;
+        this.s7 = s7;
+        this.s8 = s8;
+        this.s9 = s9;
+        this.s10 = s10;
+        this.s11 = s11;
+        this.s12 = s12;
+        this.s13 = s13;
+        this.s14 = s14;
+        this.s15 = s15;
+        this.s16 = s16;
+        this.s17 = s17;
+        this.s18 = s18;
+        this.s19 = s19;
+        this.i1 = i1;
+        this.i2 = i2;
+        this.i3 = i3;
+        this.i4 = i4;
+        this.i5 = i5;
+        this.i6 = i6;
+        this.i7 = i7;
+        this.i8 = i8;
+        this.i9 = i9;
+        this.i10 = i10;
+        this.i11 = i11;
+        this.i12 = i12;
+        this.i13 = i13;
+        this.i14 = i14;
+        this.i15 = i15;
+        this.i16 = i16;
+        this.i17 = i17;
+        this.i18 = i18;
+        this.i19 = i19;
+        this.d1 = d1;
+        this.d2 = d2;
+        this.d3 = d3;
+        this.d4 = d4;
+        this.d5 = d5;
+        this.d6 = d6;
+        this.d7 = d7;
+        this.d8 = d8;
+        this.d9 = d9;
+        this.d10 = d10;
+        this.d11 = d11;
+        this.d12 = d12;
+        this.d13 = d13;
+        this.d14 = d14;
+        this.d15 = d15;
+        this.d16 = d16;
+        this.d17 = d17;
+        this.d18 = d18;
+        this.d19 = d19;
+        this.b1 = b1;
+        this.b2 = b2;
+        this.b3 = b3;
+        this.b4 = b4;
+        this.b5 = b5;
+        this.b6 = b6;
+        this.b7 = b7;
+        this.b8 = b8;
+        this.b9 = b9;
+        this.b10 = b10;
+        this.b11 = b11;
+        this.b12 = b12;
+        this.b13 = b13;
+        this.b14 = b14;
+        this.b15 = b15;
+        this.b16 = b16;
+        this.b17 = b17;
+        this.b18 = b18;
+        this.b19 = b19;
+    }
+
+    public void set1(Object o) {
+        if (o instanceof String){
+            this.s1 = (String) o;
+        }else if (o instanceof Integer){
+            this.i1 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d1 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b1 = (Boolean) o;
+        }
+    }
+
+    public void set2(Object o) {
+        if (o instanceof String){
+            this.s2 = (String) o;
+        }else if (o instanceof Integer){
+            this.i2 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d2 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b2 = (Boolean) o;
+        }
+    }
+
+    public void set3(Object o) {
+        if (o instanceof String){
+            this.s3 = (String) o;
+        }else if (o instanceof Integer){
+            this.i3 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d3 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b3 = (Boolean) o;
+        }
+    }
+
+    public void set4(Object o) {
+        if (o instanceof String){
+            this.s4 = (String) o;
+        }else if (o instanceof Integer){
+            this.i4 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d4 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b4 = (Boolean) o;
+        }
+    }
+
+    public void set5(Object o) {
+        if (o instanceof String){
+            this.s5 = (String) o;
+        }else if (o instanceof Integer){
+            this.i5 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d5 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b5 = (Boolean) o;
+        }
+    }
+
+    public void set6(Object o) {
+        if (o instanceof String){
+            this.s6 = (String) o;
+        }else if (o instanceof Integer){
+            this.i6 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d6 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b6 = (Boolean) o;
+        }
+    }
+
+    public void set7(Object o) {
+        if (o instanceof String){
+            this.s7 = (String) o;
+        }else if (o instanceof Integer){
+            this.i7 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d7 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b7 = (Boolean) o;
+        }
+    }
+    public void set8(Object o) {
+        if (o instanceof String){
+            this.s8 = (String) o;
+        }else if (o instanceof Integer){
+            this.i8 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d8 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b8 = (Boolean) o;
+        }
+    }
+
+    public void set9(Object o) {
+        if (o instanceof String){
+            this.s9 = (String) o;
+        }else if (o instanceof Integer){
+            this.i9 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d9 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b9 = (Boolean) o;
+        }
+    }
+
+    public void set10(Object o) {
+        if (o instanceof String){
+            this.s10 = (String) o;
+        }else if (o instanceof Integer){
+            this.i10 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d10= (Double) o;
+        }else if (o instanceof Boolean){
+            this.b10 = (Boolean) o;
+        }
+    }
+
+    public void set11(Object o) {
+        if (o instanceof String){
+            this.s11 = (String) o;
+        }else if (o instanceof Integer){
+            this.i11 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d11 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b11 = (Boolean) o;
+        }
+    }
+
+    public void set12(Object o) {
+        if (o instanceof String){
+            this.s12 = (String) o;
+        }else if (o instanceof Integer){
+            this.i12 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d12 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b12 = (Boolean) o;
+        }
+    }
+
+    public void set13(Object o) {
+        if (o instanceof String){
+            this.s13 = (String) o;
+        }else if (o instanceof Integer){
+            this.i13 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d13 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b13 = (Boolean) o;
+        }
+    }
+
+    public void set14(Object o) {
+        if (o instanceof String){
+            this.s14 = (String) o;
+        }else if (o instanceof Integer){
+            this.i14 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d14 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b14 = (Boolean) o;
+        }
+    }
+
+    public void set15(Object o) {
+        if (o instanceof String){
+            this.s15 = (String) o;
+        }else if (o instanceof Integer){
+            this.i15 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d15 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b15 = (Boolean) o;
+        }
+    }
+
+    public void set16(Object o) {
+        if (o instanceof String){
+            this.s16 = (String) o;
+        }else if (o instanceof Integer){
+            this.i16 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d16 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b16 = (Boolean) o;
+        }
+    }
+
+    public void set17(Object o) {
+        if (o instanceof String){
+            this.s17 = (String) o;
+        }else if (o instanceof Integer){
+            this.i17 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d17 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b17 = (Boolean) o;
+        }
+    }
+    public void set18(Object o) {
+        if (o instanceof String){
+            this.s18 = (String) o;
+        }else if (o instanceof Integer){
+            this.i18 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d18 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b18 = (Boolean) o;
+        }
+    }
+
+    public void set19(Object o) {
+        if (o instanceof String){
+            this.s19 = (String) o;
+        }else if (o instanceof Integer){
+            this.i19 = (Integer) o;
+        }else if (o instanceof Double){
+            this.d19 = (Double) o;
+        }else if (o instanceof Boolean){
+            this.b19 = (Boolean) o;
+        }
+    }
+
+
+    public String get1s() { return s1; }
+    public Integer get1i() { return i1; }
+    public Double get1d() { return d1; }
+    public Boolean get1b() { return b1; }
+
+    public String get2s() { return s2; }
+    public Integer get2i() { return i2; }
+    public Double get2d() { return d2; }
+    public Boolean get2b() { return b2; }
+
+    public String get3s() { return s3; }
+    public Integer get3i() { return i3; }
+    public Double get3d() { return d3; }
+    public Boolean get3b() { return b3; }
+
+    public String get4s() { return s4; }
+    public Integer get4i() { return i4; }
+    public Double get4d() { return d4; }
+    public Boolean get4b() { return b4; }
+
+    public String get5s() { return s5; }
+    public Integer get5i() { return i5; }
+    public Double get5d() { return d5; }
+    public Boolean get5b() { return b5; }
+
+    public String get6s() { return s6; }
+    public Integer get6i() { return i6; }
+    public Double get6d() { return d6; }
+    public Boolean get6b() { return b6; }
+
+    public String get7s() { return s7; }
+    public Integer get7i() { return i7; }
+    public Double get7d() { return d7; }
+    public Boolean get7b() { return b7; }
+
+    public String get8s() { return s8; }
+    public Integer get8i() { return i8; }
+    public Double get8d() { return d8; }
+    public Boolean get8b() { return b8; }
+
+    public String get9s() { return s9; }
+    public Integer get9i() { return i9; }
+    public Double get9d() { return d9; }
+    public Boolean get9b() { return b9; }
+
+    public String get10s() { return s10; }
+    public Integer get10i() { return i10; }
+    public Double get10d() { return d10; }
+    public Boolean get10b() { return b10; }
+
+    public String get11s() { return s11; }
+    public Integer get11i() { return i11; }
+    public Double get11d() { return d11; }
+    public Boolean get11b() { return b11; }
+
+    public String get12s() { return s12; }
+    public Integer get12i() { return i12; }
+    public Double get12d() { return d12; }
+    public Boolean get12b() { return b12; }
+
+    public String get13s() { return s13; }
+    public Integer get13i() { return i13; }
+    public Double get13d() { return d13; }
+    public Boolean get13b() { return b13; }
+
+    public String get14s() { return s14; }
+    public Integer get14i() { return i14; }
+    public Double get14d() { return d14; }
+    public Boolean get14b() { return b14; }
+
+    public String get15s() { return s15; }
+    public Integer get15i() { return i15; }
+    public Double get15d() { return d15; }
+    public Boolean get15b() { return b15; }
+
+    public String get16s() { return s16; }
+    public Integer get16i() { return i16; }
+    public Double get16d() { return d16; }
+    public Boolean get16b() { return b16; }
+
+    public String get17s() { return s17; }
+    public Integer get17i() { return i17; }
+    public Double get17d() { return d17; }
+    public Boolean get17b() { return b17; }
+
+    public String get18s() { return s18; }
+    public Integer get18i() { return i18; }
+    public Double get18d() { return d18; }
+    public Boolean get18b() { return b18; }
+
+    public String get19s() { return s19; }
+    public Integer get19i() { return i19; }
+    public Double get19d() { return d19; }
+    public Boolean get19b() { return b19; }
+
+
+}
